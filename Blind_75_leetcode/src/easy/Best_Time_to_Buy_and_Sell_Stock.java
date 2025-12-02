c55d320d3af50ce44dc24c4a3994180647fd47ee
Kino <nnnnguyentuankiet@gmail.com>
2025-12-02 16:03:30 +0700

    U-L2- add comment

--- Blind_75_leetcode/src/easy/Best_Time_to_Buy_and_Sell_Stock.java
+++ Blind_75_leetcode/src/easy/Best_Time_to_Buy_and_Sell_Stock.java
@@ -9,21 +9,30 @@ package easy;
  * @author D E L L
  */
 public class Best_Time_to_Buy_and_Sell_Stock {
-      public int maxProfit(int[] prices) {
+
+    public int maxProfit(int[] prices) {
+        //các case ban đầu(bắt đầu)
         int buy = prices[0];
-          int sell = 0;
-          int profit = 0;
-          for (int i = 1; i < prices.length; i++) {
-              if(prices[i]<buy){
-                  buy = prices[i];
-                  sell = prices[i];
-              }else{
-                  sell = prices[i];
-              }
-              if(sell-buy>profit){
-                  profit = sell - buy;
-              }  
-          }
+        int sell = 0;
+        int profit = 0;
+        
+        
+        /*
+        Khi gặp giá [trị nhỏ hơn] giá trị mua vào lập tức để giá trị mua và bán tại [giá trị nhỏ hơn] đó
+        Nếu giá trí tiếp theo được quét lớn hơn giá trị mua vào và profit sau tính toán lớn hơn profit hiện tại--> thay đổi giá trị profit
+        nếu không thì tiếp tục quét phần tử tiếp theo(bỏ qua việc cập nhật profit)
+        */
+        for (int i = 1; i < prices.length; i++) { 
+            if (prices[i] < buy) {
+                buy = prices[i];
+                sell = prices[i];
+            } else {
+                sell = prices[i];
+            }
+            if (sell - buy > profit) {
+                profit = sell - buy;
+            }
+        }
         return profit;
     }
 }
