58a1ebfc067fc62b56865b0ab9079ecffd90286a
Kino <nnnnguyentuankiet@gmail.com>
2025-12-04 17:17:08 +0700

    U-L4 Không dùng phép chia

--- Blind_75_leetcode/src/easy/Product_of_Array_Except_Self.java
+++ Blind_75_leetcode/src/easy/Product_of_Array_Except_Self.java
@@ -8,45 +8,24 @@ package easy;
  *
  * @author D E L L Đề bài yêu cầu cho một mảng nums Tính ra mảng result với từng
  * phần tử là tích các số trừ vị trí index hiện tải của mảng ban đầu vd
- * nums[1,2,3,4] result[24,12,8,6] 24 = 2*3*4(ngoại trừ 1) có 3 trường hợp có
- * thể xảy ra: 1- mảng không chứa phần tử 0 2- mảng chứa 1 phần tử 0 3- mảng
- * chứa 2 số 0 trở lên
+ * nums[1,2,3,4] result[24,12,8,6] 24 = 2*3*4(ngoại trừ 1)
  */
 public class Product_of_Array_Except_Self {
 
     public int[] productExceptSelf(int[] nums) {
-        int zeroCount = 0;
-        int[] result = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                zeroCount += 1;
-            }
+        int sizeOfNums = nums.length;
+        int[] results = new int[sizeOfNums];
+        //base 
+        results[0] = 1;
+        for (int i = 1; i < sizeOfNums; i++) {
+            results[i] = results[i - 1] * nums[i - 1];
         }
-        if (zeroCount == 1) {
-            int productResult = 1;
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] != 0) {
-                    productResult *= nums[i];
-                }
-            }
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] == 0) {
-                    result[i] = productResult;
-                } else {
-                    result[i] = 0;
-                }
-            }
-        } else if (zeroCount == 0) {
-            int productResult = 1;
-            for (int i = 0; i < nums.length; i++) {
-                productResult *= nums[i];
-            }
-            for (int i = 0; i < nums.length; i++) {
-                result[i] = productResult / nums[i];
-            }
-        } else {
-            return result;
+        int suffix = 1;
+        for (int i = sizeOfNums - 1; i >= 0; i--) {
+            results[i] *= suffix;
+            suffix *= nums[i];
         }
-        return result;
+
+        return results;
     }
 }
