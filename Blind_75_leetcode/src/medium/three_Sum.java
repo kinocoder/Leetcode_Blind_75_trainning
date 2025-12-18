4284548716e615df11ec2f2cdab36c8de8594c5d
Kino <nnnnguyentuankiet@gmail.com>
2025-12-18 16:46:49 +0700

    A_L9 3 Sum

---
+++ Blind_75_leetcode/src/medium/three_Sum.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * @author D E L L
+ */
+public class three_Sum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i <= nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    resultList.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }else if(sum < 0){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+        }
+        return resultList;
+    }
+}
